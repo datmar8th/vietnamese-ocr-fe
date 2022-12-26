8efa364686a57c7225e9012904dc320184a3a6a8
Truong Cong Thanh Dat <datmar8th@gmail.com>
2022-12-27 02:51:07 +0700

    .

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -35,7 +35,7 @@ const Feature = () => {
     formData.append("file", imageFile, "img_transformer.png");
 
     let t0 = performance.now();
-    axios.post("https://66b6-14-236-7-220.ap.ngrok.io/upload", formData).then((res, data) => {
+    axios.post("https://b05c-14-236-7-220.ap.ngrok.io/upload", formData).then((res, data) => {
       data = res.data;
       setImagePrediction(data);
       let t1 = performance.now();
